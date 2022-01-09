f5f9fb3bb63fa355fcb6d1367060bd9dc513354d
Abdullah Alansari <ahimta@gmail.com>
2022-01-10 00:35:12 +0300

    chore: suppress system sound-notification error-message
    And use a more friendly error-message than the one we used before.

--- scripts/notify-user.sh
+++ scripts/notify-user.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-mpv --really-quiet /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga ||
-  echo "can't play sound notification because mpv isn't installed" >&2
+mpv --really-quiet /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga 2>/dev/null ||
+  echo "can't play sound notification." >&2
