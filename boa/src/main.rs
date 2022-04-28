48c5b07ac3797207eba7f7db10e96cd49c6a195f
Abdullah Alansari <ahimta@gmail.com>
2022-04-28 05:02:49 +0300

    change(boa): remove redundant data text-format
    Namely the eliminated-teams that are now part of the elimination-status.

--- boa/src/main.rs
+++ boa/src/main.rs
@@ -1,5 +1,3 @@
-use boa::tournament_prediction::EliminationStatus;
-
 fn main() {
   boa::test();
 
@@ -8,34 +6,24 @@ fn main() {
     println!("|{:-^116}|", "");
     println!("|{:^116}|", tournament.name);
     println!("|{:-^116}|", "");
-    println!("| {rank:4} | {id:25} | {eliminated:11} | {matches_left:12} | {matches_won:11} | {eliminating_teams:36} |",
+    println!("| {rank:4} | {id:25} | {eliminated:52} | {matches_left:12} | {matches_won:11} |",
   rank="Rank",
   id="ID",
   eliminated="Eliminated",
   matches_left="Matches Left",
   matches_won="Matches Won",
-  eliminating_teams="Eliminating Teams",
     );
     println!("|{:-^116}|", "");
 
     for team in boa::tournament_prediction::predict_tournament_eliminated_teams(
       tournament,
     ) {
-      let eliminating_teams =
-        match EliminationStatus::clone(&team.elimination_status) {
-          EliminationStatus::Not => vec![].into_iter().collect(),
-          EliminationStatus::Trivially(eliminating_teams)
-          | EliminationStatus::NonTrivially(eliminating_teams) => {
-            eliminating_teams
-          }
-        };
-      println!("| {rank:4} | {id:25} | {elimination_status:11?} | {matches_left:12} | {matches_won:11} | {eliminating_teams:36} |",
+      println!("| {rank:4} | {id:25} | {elimination_status:52?} | {matches_left:12} | {matches_won:11} |",
 rank=team.rank,
 id=team.id,
 elimination_status=team.elimination_status,
 matches_left=team.matches_left,
 matches_won=team.matches_won,
-eliminating_teams=eliminating_teams.iter().map(|team_id|String::clone(team_id)).collect::<Vec<_>>().join(", "),
     );
     }
   }
