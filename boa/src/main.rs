ed810b6a9d04b37caa020055ce2926bbe537a23b
Abdullah Alansari <ahimta@gmail.com>
2022-04-28 05:19:24 +0300

    change(boa): move long columns last
    Namely elimination-status in text-format.

--- boa/src/main.rs
+++ boa/src/main.rs
@@ -6,24 +6,24 @@ fn main() {
     println!("|{:-^116}|", "");
     println!("|{:^116}|", tournament.name);
     println!("|{:-^116}|", "");
-    println!("| {rank:4} | {id:25} | {eliminated:52} | {matches_left:12} | {matches_won:11} |",
+    println!("| {rank:4} | {id:25} | {matches_left:12} | {matches_won:11} | {eliminated:50} |",
   rank="Rank",
   id="ID",
-  eliminated="Eliminated",
   matches_left="Matches Left",
   matches_won="Matches Won",
+  eliminated="Eliminated",
     );
     println!("|{:-^116}|", "");
 
     for team in boa::tournament_prediction::predict_tournament_eliminated_teams(
       tournament,
     ) {
-      println!("| {rank:4} | {id:25} | {elimination_status:52?} | {matches_left:12} | {matches_won:11} |",
+      println!("| {rank:4} | {id:25} | {matches_left:12} | {matches_won:11} | {elimination_status:50?} |",
 rank=team.rank,
 id=team.id,
-elimination_status=team.elimination_status,
 matches_left=team.matches_left,
 matches_won=team.matches_won,
+elimination_status=team.elimination_status,
     );
     }
   }
